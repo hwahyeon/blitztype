1bf2468739109363b9ff6b7c0524c5d92074b753
Janghyeon <hwahyeon.dev@gmail.com>
2024-08-15 23:10:36 +0200

    feat: Create 8 umlaut buttons

---
+++ main.lisp
@@ -0,0 +1,26 @@
+(ql:quickload :ltk)
+(use-package :ltk)
+
+(defun buttons()
+  (with-ltk ()
+   (let* ((f (make-instance 'frame))
+          (umlauts '(("Ä" "ä") ("Ö" "ö") ("Ü" "ü") ("Ë" "ë"))))
+     
+     (pack f)
+
+
+     (dolist (row umlauts)
+       (let ((row-frame (make-instance 'frame :master f))) 
+         (dolist (char row)
+           (let ((button (make-instance 'button
+                                        :master row-frame
+                                        :text char
+                                        :command (lambda ()
+                                                   (format t "~A 버튼이 클릭되었습니다!~&" char)))))
+             (pack button :side :left)))
+         (pack row-frame :side :top)))
+
+     (configure f :borderwidth 3)
+     (configure f :relief :sunken))))
+
+(buttons)
