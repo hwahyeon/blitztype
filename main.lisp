0831ba5a03014c9e50cf14a07f356cacee2af408
Janghyeon <hwahyeon.dev@gmail.com>
2024-08-16 00:43:53 +0200

    feat: Add information label

--- main.lisp
+++ main.lisp
@@ -3,27 +3,25 @@
 
 (defun buttons()
   (with-ltk ()
-    (wm-title *tk* "Copy Umlaut")
-            
-   (let* ((f (make-instance 'frame))
-          (umlauts '(("Ä" "ä") ("Ö" "ö") ("Ü" "ü") ("Ë" "ë"))))
-     
-     (pack f)
+    (wm-title *tk* "Umlaut Buttons")
 
-     (dolist (row umlauts)
-       (let ((row-frame (make-instance 'frame :master f))) 
-         (dolist (char row)
-           (let ((button (make-instance 'button
-                                        :master row-frame
-                                        :text char
-                                        :command (lambda ()
-                                                   (progn
-                                                      ;; Clipboard
-                                                      (format-wish "clipboard clear; clipboard append ~S" char))))))
-             (pack button :side :left)))
-         (pack row-frame :side :top)))
+    (let* ((f (make-instance 'frame))
+           (info-label (make-instance 'label :master f :text "Click a button to copy")))
 
-     (configure f :borderwidth 3)
-     (configure f :relief :sunken))))
+      (dolist (row '(("Ä" "ä") ("Ö" "ö") ("Ü" "ü") ("Ë" "ë")))
+        (let ((row-frame (make-instance 'frame :master f)))
+          (dolist (char row)
+            (let ((button (make-instance 'button
+                                         :master row-frame
+                                         :text char
+                                         :command (lambda ()
+                                                    (format-wish "clipboard clear; clipboard append ~S" char)))))
+              (pack button :side :left)))
+          (pack row-frame :side :top)))
+
+      (pack info-label :side :bottom :fill :x)
+      (configure info-label :anchor :center)
+
+      (pack f :side :top :fill :both :expand t))))
 
 (buttons)
