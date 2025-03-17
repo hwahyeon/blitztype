615e2dd4b38644a07a08ad59b440c954a8e307d8
Janghyeon <hwahyeon.dev@gmail.com>
2025-03-17 23:41:54 +0100

    feat: Add GUI initialization header file

---
+++ src/gui.h
@@ -0,0 +1,20 @@
+#ifndef GUI_H
+#define GUI_H
+
+#include <windows.h>
+
+#define ID_BUTTON_TOGGLE 1
+#define ID_LINK_ISSUE 2
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void InitGUI(HINSTANCE hInstance, int nCmdShow);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
