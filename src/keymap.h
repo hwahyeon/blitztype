617beeaf16364e04b05e2a18d00c1f382d0f2079
Janghyeon <hwahyeon.dev@gmail.com>
2025-08-04 22:42:05 +0200

    feat: Add keymap header with start and stop function declarations

---
+++ src/keymap.h
@@ -0,0 +1,18 @@
+#ifndef KEYMAP_H
+#define KEYMAP_H
+
+#include <windows.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void StartKeymap();
+    void StopKeymap();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
